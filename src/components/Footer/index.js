74b159421fd354c627f46301fe75bb667893e702
Onezimo Fontes Costa Junior <Onezimo.Junior@heal.local>
2020-07-29 11:24:39 -0300

    Commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.devflix.nzm/">
+        <img src={Logo} alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Vídeos de
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="https://www.devflix.nzm">
+        Desenvolvimento
         </a>
       </p>
     </FooterBase>
